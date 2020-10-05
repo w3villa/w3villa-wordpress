3cff0f6f31ea5786290864fdeaac7d3c33519f96
w3villa-awanish <67098585+w3villa-awanish@users.noreply.github.com>
2020-10-05 16:22:24 +0530

    Update creates_sidebar.php

--- creates_sidebar.php
+++ creates_sidebar.php
@@ -1,7 +1,7 @@
 function wpdocs_theme_slug_widgets_init() {
     register_sidebar( array(
         'name'          => __( 'Main Sidebar', 'textdomain' ),
-        'id'            => 'sidebar-1',
+        'id'            => 'your-sidebar-slug',
         'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'textdomain' ),
         'before_widget' => '<li id="%1$s" class="widget %2$s">',
         'after_widget'  => '</li>',
@@ -10,3 +10,8 @@ function wpdocs_theme_slug_widgets_init() {
     ) );
 }
 add_action( 'widgets_init', 'wpdocs_theme_slug_widgets_init' );
+
+
+//Get sidebar
+
+dynamic_sidebar('your-sidebar-slug');
