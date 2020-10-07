4a47a98d72b2f24a4d34fdaf2a3f6ae3df616684
w3villa-awanish <67098585+w3villa-awanish@users.noreply.github.com>
2020-10-07 17:08:37 +0530

    Add files via upload

---
+++ create-theme/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<div id="footer">© <?php date('Y'); ?> <?php bloginfo('name'); ?> Theme is copyrighted to W3villa.</div>
+</footer>
+</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
