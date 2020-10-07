38deeceb045ccfb8c9f70785254092689a7abdfe
Anchal Gupta <60260296+w3villa-anchal@users.noreply.github.com>
2020-10-07 16:48:06 +0530

    Delete footer.php

--- create-theme/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-<div id="footer">© <?php date('Y'); ?> <?php bloginfo('name'); ?> Theme is copyrighted to W3villa.</div>
-</footer>
-</div>
-</div>
-<?php wp_footer(); ?>
-</body>
-</html>
